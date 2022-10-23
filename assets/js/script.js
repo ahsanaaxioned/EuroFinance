c2dacb681a35089f427806d3d3b258b11cd7c97b
ahsanaaxioned <ahsana@axioned.com>
2022-10-24 04:19:42 +0530

    js all input validation done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 // global variable declaration start here
-const hamburger = document.querySelector(".hamburger"),
+const form = document.querySelector(".form")
+    hamburger = document.querySelector(".hamburger"),
     navbar = document.querySelector(".navbar"),
     html = document.querySelector('html'),
     firstName = document.querySelector(".first"),
-    lastName= document.querySelector(".last")
-    inputGroup = document.querySelector(".input-group")
-input = document.querySelector(".input");
-nameRegex = /^([a-zA-Z]){3,12}$/;
-let counter = 0;
+    lastName = document.querySelector(".last"),
+    position = document.querySelector(".position"),
+    company = document.querySelector(".company"),
+    email = document.querySelector(".email"),
+    choose = document.querySelector(".company-type"),
+    country = document.querySelector(".country"),
+    scrollTop = document.querySelector(".scrollTop-btn");
+    nameRegex = /^([a-zA-Z])/,
+    emailRegex = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
 // global variable declaration start here
 // hambuger function  start here
 hamburger.addEventListener("click", function () {
@@ -16,108 +21,115 @@ hamburger.addEventListener("click", function () {
     html.classList.toggle("html")
 });
 // hambuger function  end here
-function validation(inputName,regex){
-    const str = inputName.value;
-if(str ==""){
-    spanError.classList.add("input-span");
-    spanError.innerText="please eneter a name"
-}
-else if(!regex.test(str) && str.length<3){
-    spanError.classList.add("input-span");
-    spanError.innerText="please eneter a valid name and it should be atleast 3 caharacter"
-}
-else{
-    spanError.classList.remove("input-span");
-    spanError.innerText=""
-}
+// form submit event start here
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    error = document.querySelectorAll("succes");
+    if ((firstName.value) && (lastName.value) && (position.value) && (company.value) && (choose.value) && (country.value) && (email.value) && (yes.checked || no.checked) && (error.length === 0)) {
+        alert("Your form has been submitted successfully..!");
+        firstName.value = "";
+        lastName.value = "";
+        position.value = "";
+        company.value = "";
+        choose.value = "";
+        country.value = "";
+        yes.checked = false;
+        no.checked = false;
+    } else {
+        validation(firstName, nameRegex);
+        validation(lastName, nameRegex);
+        validation(position, nameRegex);
+        validation(company, nameRegex);
+        pick(choose);
+        pick(country);
+        validation(email, emailRegex);
+        checkBox();
+    }
+});
+// form submit event end here
+// validation function start here
+function validation(input, regex) {
+    const inputGroup = input.parentElement,
+        spanError = inputGroup.querySelector(".input-span"),
+        str = input.value;
+    if (str == "") {
+        spanError.classList.add("fail");
+        inputGroup.classList.add("error");
+        spanError.innerText = "*please enter a valid " + input.name;
+    }else if (!regex.test(str)) {
+        spanError.classList.add("fail");
+        inputGroup.classList.add("error");
+        spanError.innerText = "*please eneter a valid " + input.name;
+    }else if (str.length < 4) {
+        spanError.classList.add("fail");
+        inputGroup.classList.add("error");
+        spanError.innerText = "*it must be atleast 4 character";
+    }else {
+        spanError.classList.remove("fail");
+        inputGroup.classList.add("succes");
+        inputGroup.classList.remove("error");
+        pick(choose);
+    }
+};
 
-}
-firstName.addEventListener("blur",function(){
-    validation(firstName,nameRegex);
+function pick(optionInput) {
+    const opt = optionInput.parentElement,
+        spanError = opt.querySelector(".input-span");
+    if (optionInput.value == "select") {
+        spanError.classList.add("fail");
+        spanError.innerText = "*please select a valid option";
+    } else {
+        spanError.classList.remove("fail");
+    }
+};
+function checkBox() {
+    spanError = document.querySelector(".yes-input");
+    yes = document.querySelector(".yes"),
+        no = document.querySelector(".no");
+    if ((yes.checked != true) && (no.checked != true)) {
+        spanError.classList.add("fail");
+        spanError.innerText = "*please select our T&C "
+    } else {
+        spanError.classList.remove("fail");
+        spanError.innerText = "";
+        console.log("HI CHECKBOX");
+    }
+};
+// validation function end here
+// input blur event start here
+firstName.addEventListener("blur", function () {
+    validation(firstName, nameRegex);
 });
 
-lastName.addEventListener("blur",function(){
-    validation(lastName,nameRegex);
+lastName.addEventListener("blur", function () {
+    validation(lastName, nameRegex);
 });
 
+position.addEventListener("blur", function () {
+    validation(position, nameRegex);
+});
 
+company.addEventListener("blur", function () {
+    validation(company, nameRegex);
+});
 
+email.addEventListener("blur", function () {
+    validation(email, emailRegex);
+});
 
+choose.addEventListener("blur", function () {
+    pick(choose);
+});
 
-
-
-
-
-
-
-// // let parentEle = input.parentElement
-// // inputGroup.forEach(function(element,index){
-// //     console.log(element.children);
-// // })
-// function validation(input, errorMsg, regax) {
-//     let spanError;
-//     if (input.value == "" || !regax.test(input.value)) {
-//         const err = document.querySelector('.input-span')
-//         if (!err) {
-
-//             spanError = document.createElement("span")
-//             spanError.innerText = errorMsg
-//             inputGroup.appendChild(spanError);
-//             spanError.classList.add("input-span")
-//         }
-
-//     } else {
-//         spanError.innerText='someting bad input'
-//     }
-
-
-// }
-
-
-// firstName.addEventListener("focus", function (e) {
-//     e.preventDefault()
-//     validation(firstName, "*plese eneter a valid first name", nameRegex);
-//     // console.warn('focus triger',firstName)
-//     // firstName.parentElement
-// });
-// // console.log( inputt.parentELement);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+country.addEventListener("blur", function () {
+    pick(country);
+});
+// input blur event start here
+// scroll to the top start here
+scrollTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+});
+// scroll to the top end here
